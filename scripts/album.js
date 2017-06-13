bfeabf0ab31c5ef77daafcaa90cb9f00c750c9f1
Christopher Sporn <christophersporn@Christophers-iMac.local>
2017-06-13 05:55:35 -0400

    checkpoint-13-pause: completed

--- scripts/album.js
+++ scripts/album.js
@@ -158,14 +158,23 @@ var setCurrentAlbum = function(album) {
               if(event.target.parentElement.className === 'album-view-song-item'){
 
               event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+              var songItem = getSongItem(event.target);
+
+              if(songItem.getAttribute('data-song-number') !== currentlyPlayingSong){
+                  songItem.innerHTML = playButtonTemplate;
+                }
               }
          });
 
          for (var i = 0; i < songRows.length; i++) {
             songRows[i].addEventListener('mouseleave', function(event) {
-
-                // Selects first child element, which is the song-item-number element
-                this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+              //#1
+                var songItem = getSongItem(event.target);
+                var songItemNumber = songItem.getAttribute('data-song-number');
+              //#2
+                if(songItemNumber !== currentlyPlayingSong){
+                  songItem.innerHTML = songItemNumber;
+                }
             });
 
             songRows[i].addEventListener('click', function(event){
