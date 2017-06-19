79bed52f781ec71e2987d889b85b6a818c38ae7f
Christopher Sporn <christophersporn@Christophers-iMac.local>
2017-06-18 23:03:15 -0400

    checkpoint-16-collectionQuery:completed

--- scripts/album.js
+++ scripts/album.js
@@ -56,39 +56,41 @@ var createSongRow = function(songNumber, songName, songLength) {
      + '</tr>'
      ;
 
-    return template;
+    return $(template);
 };
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+    var $albumTitle = $('.album.view-title');
+    var $albumArtist = $('.album-view-artist');
+    var $albumReleaseInfor = $('.album-view-release-info');
+    var $albumImage = $('.album-cover-art');
+    var $albumSongList = $('.album-view-song-list');
 
 
 var setCurrentAlbum = function(album) {
      // #1
-       albumTitle = document.getElementsByClassName('album-view-title')[0];
-       albumArtist = document.getElementsByClassName('album-view-artist')[0];
-       albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-       albumImage = document.getElementsByClassName('album-cover-art')[0];
-       albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+       $albumTitle = $('.album-view-title');
+       $albumArtist = $('.album-view-artist');
+       $albumReleaseInfo = $('.album-view-release-info');
+       $albumImage = $('.album-cover-art');
+       $albumSongList = $('.album-view-song-list');
 
      // #2
-      albumTitle.innerText = album.title;
-      albumArtist.innerText= album.artist;
-      albumReleaseInfo.innerText=album.year + ' ' + album.label;
-      albumImage.setAttribute('src', album.albumArtUrl);
+      $albumTitle.text(album.title);
+      $albumArtist.text(album.artist);
+      $albumReleaseInfo.text(album.year + ' ' + album.label);
+      $albumImage.attr('src', album.albumArtUrl);
 
 
      // #3
-     albumSongList.innerHTML = '';
+     $albumSongList.empty();
 
      // #4
      for (var i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+         var $newRow = createSongRow(i+1, album.songs[i].title, album.songs[i].duration);
+         $albumSongList.append($newRow);
      }
  };
-  
+
    var songRows = document.getElementsByClassName('album-view-song-item');
    // album button template
    var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
